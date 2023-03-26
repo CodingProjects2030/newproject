660018ec7b03d4432f2afa1a914671930c38adfd
CodingProjects2030 <126919560+CodingProjects2030@users.noreply.github.com>
2023-03-26 03:05:00 +0300

    ملف قاعدة ابليانات

--- DbConnect.php
+++ DbConnect.php
@@ -1,6 +1,6 @@
 <?php
 
- $conn=mysqli_connect("localhost","root","","motivation");
+ $conn=mysqli_connect("localhost","root","","insistence");
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
