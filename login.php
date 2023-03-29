2c14bcb5f2b80f8fab29a36a8d3e0fbc87caf9b9
CodingProjects2030 <126919560+CodingProjects2030@users.noreply.github.com>
2023-03-29 23:05:09 +0300

    تخديث المنيو في صفحة التسجيل

--- login.php
+++ login.php
@@ -10,24 +10,102 @@
   <link href="Student.css?session=7" rel="stylesheet">
    <link href="cs.css?=version=6" rel="stylesheet">
   <script type="text/javascript" src="Student.js"></script>
+<style>
+.dropbtn {
+  color: #151414;
 
+font-size: 22px;
+border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  
+border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {}
+
+.dropdown:hover .dropdown-content {display: block;position:fixed;}
+
+.dropdown:hover .dropbtn {}
+
+
+</style>
 </head>
 <body>
 
 <div class="top">
 <ul>
-   
-     <li class="h"><a href="#about"><img src="imgs/logo.png" height="120" width="230"></a></li><br><br>
-  <li class="hh"><a href="indexen.html">English</a></li>
-  <li class="hh"><a href="contactar.html">ولي الامر</a></li>
-  
-    <li class="hh"><a href="contactar.php">الخدمات الصحية</a></li>
- <li class="hh"><a href="login.html">الخدمات التعليمية</a></li>
-<li class="hh"><a href="TeacherAr.html">الخدمات الترفيهية</a></li>
-
-  
+ 
+  <li class="h"><a href="#about"><img src="imgs/logo.png" height="120" width="230"></a></li><br><br>
+ 
+    <?php
+if(isset($_SESSION['userid']) && !empty($_SESSION['userid'])) {
+echo '<li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;" ><a href="signout.php">تسجيل الخروج</a></li>';
+echo '<li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;"><a href="update.php">تحديث البيانات</a></li>';
+}
+else
+{   ?>
 	
+	<?php
+	
+}
+?>
+  <li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;" ><a href="parentar.php">ولي الامر</a></li>
   
+    <li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;" ><a href="healthservices.php">الخدمات الصحية</a></li>
+	
+
+ <li class="hh" style="
+margin-left: 8px;" ><div class="dropdown"> <a class="dropbtn" href="educationalservices.php">الخدمات التعليمية <div class="dropdown-content">
+    <a href="#">اللغة الانجليزية</a>
+    <a href="#">تقنية معلومات</a>
+    <a href="#">اي شي </a>
+  </div></a></div></li>
+<li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;" ><a href="entertainmentservices.php">الخدمات الترفيهية</a></li>
+
+   <li class="hh" style="border: 2px solid black;
+background-color: white;
+border-radius: 20px;
+margin-left: 8px;" ><a href="indexen.html">English</a></li>
+  
+   
+
 </ul>
 </div>
 
